ec7bb8a18753c0d6136c0861fbfd1995fb12f7a0
@Jisan09 <mdjisan7509@gmail.com>
2022-02-10 19:36:02 +0530

    Update session.py

--- userbot/core/session.py
+++ userbot/core/session.py
@@ -8,7 +8,7 @@ from .client import CatUserBotClient
 
 __version__ = "2.10.6"
 
-loop = None
+
 
 if Config.STRING_SESSION:
     session = StringSession(str(Config.STRING_SESSION))
@@ -20,11 +20,9 @@ try:
         session=session,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        loop=loop,
         app_version=__version__,
-        #connection=ConnectionTcpAbridged,
         auto_reconnect=True,
-        connection_retries=None,
+        connect_retries=None,
     )
 except Exception as e:
     print(f"STRING_SESSION - {e}")
@@ -35,9 +33,7 @@ catub.tgbot = tgbot = CatUserBotClient(
     session="CatTgbot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
-    #loop=loop,
     app_version=__version__,
-    #connection=ConnectionTcpAbridged,
     auto_reconnect=True,
-    connection_retries=None,
+    connect_retries=None,
 ).start(bot_token=Config.TG_BOT_TOKEN)
